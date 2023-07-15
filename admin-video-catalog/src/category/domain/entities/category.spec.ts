ae923c30f3cfc971bf03b6914bac9a4ca7bff479
milena-mognon <milenamognon@gmail.com>
2023-07-15 16:41:31 -0300

    feat: category entity abd test

--- admin-video-catalog/src/category/domain/entities/category.spec.ts
+++ admin-video-catalog/src/category/domain/entities/category.spec.ts
@@ -2,8 +2,28 @@ import { Category } from "./category";
 
 describe("Category Unit Tests", () => {
   test("constructor of category", () => {
-    const category = new Category("Movie");
+    // Triple A - Três etapas de um teste -- Arrange - Act - Assert
 
-    expect(category.name).toBe("Movie");
+    // Arrange
+    const categoryProps = {
+      name: "Movie",
+      description: "Movie Category Description",
+      is_active: true,
+      created_at: new Date(),
+    };
+
+    //  Act
+    const category = new Category(categoryProps);
+
+    // Assert
+    // toBe - compara apenas valores
+    expect(category.name).toBe(categoryProps.name);
+    expect(category.description).toBe(categoryProps.description);
+    expect(category.is_active).toBeTruthy();
+    expect(category.created_at).toBe(categoryProps.created_at);
+
+    // toMatchObject - compara objetos de forma parcial
+    // toStrictEqual - compara propriedades e valores, precisa ser exatamente igual
+    expect(category.props).toStrictEqual(categoryProps);
   });
 });
